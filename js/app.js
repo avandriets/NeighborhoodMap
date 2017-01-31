a18063215872a83041a2e816f5c5fd0fcd4278f5
AVAndriets <avandriets@gmail.com>
2017-01-31 03:01:55 +0200

    work on mistakes

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ var infowindow;
 // Init data for list and map
 var locationMarkers = [
     {
-        name: "Kiev Pechersk Lavra",
+        name: 'Kiev Pechersk Lavra',
         position: {lat: 50.434167, lng: 30.559167}
     },
     {
-        name: "Mother Motherland, Kiev",
+        name: 'Mother Motherland, Kiev',
         position: {lat: 50.426521, lng: 30.563187}
     },
     {
@@ -16,24 +16,19 @@ var locationMarkers = [
         position: {lat: 50.443889, lng: 30.518056}
     },
     {
-        name: "Vydubychi Monastery",
+        name: 'Vydubychi Monastery',
         position: {lat: 50.4168, lng: 30.5674}
     },
     {
-        name: "Saint Vladimir Hill",
+        name: 'Saint Vladimir Hill',
         position: {lat: 50.456772, lng: 30.5263}
     }
 ];
 
-// to ask if map loaded or nor
-var map_timeout = setTimeout(function () {
-    if (!window.google || !window.google.maps) {
-        //$("#loading_map_error").css("display", "block");
-        document.getElementById('loading_map_error').style.display = 'block';
-        console.log("Error loading map");
-    }
-}, 5000);
-
+function mapErrorHandler() {
+    document.getElementById('loading_map_error').style.display = 'block';
+    console.log('Error loading map');
+}
 
 //Init google map class
 function initMap() {
@@ -43,12 +38,17 @@ function initMap() {
         zoom: 13
     });
 
+    infowindow = new google.maps.InfoWindow({
+        content: ""
+    });
+
     //first init marker array
     locationsModel.filteredLocations().forEach(function (element) {
         var marker = new google.maps.Marker({
             position: {lat: element.lat, lng: element.lng},
             map: map,
-            title: element.name
+            title: element.name,
+            animation: google.maps.Animation.DROP,
         });
 
         element.marker = marker;
@@ -59,14 +59,6 @@ function initMap() {
 
     });
 }
-
-//JQuery resize function for resizing map
-$(window).resize(function () {
-    var h = $(window).height(),
-        offsetTop = 60; // Calculate the top offset
-    $('#map').css('height', (h - offsetTop));
-}).resize();
-
 
 // Class to represent a row in the seat reservations grid
 function LocationsStore(data) {
@@ -81,7 +73,7 @@ function LocationsStore(data) {
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
     locationsModel.locationsList().forEach(function (element) {
-        element.marker.setMap(null);
+        element.marker.setVisible(false);
     });
 }
 
@@ -95,13 +87,9 @@ function showInfoWindow(title, description, url, element) {
 
     if (infowindow) {
         infowindow.close();
+        infowindow.setContent(contentString);
+        infowindow.open(map, element.marker);
     }
-
-    infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    infowindow.open(map, element.marker);
 }
 
 // Overall viewmodel for this screen, along with initial state
@@ -112,7 +100,7 @@ function LocationsViewModel() {
     self.selectedLocation = ko.observable();
 
     //Search string
-    self.query = ko.observable("");
+    self.query = ko.observable('');
 
     self.locationsList = ko.observableArray([]);
 
@@ -120,14 +108,29 @@ function LocationsViewModel() {
         self.locationsList.push(new LocationsStore(element))
     });
 
+    self.bounceMarker = function () {
+        self.locationsList().forEach(function (element) {
+            element.marker.setAnimation(null);
+        });
+
+        if (self.selectedLocation) {
+            if (self.selectedLocation.marker.getAnimation() !== null) {
+                self.selectedLocation.marker.setAnimation(null);
+            } else {
+                self.selectedLocation.marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        }
+    };
+
     // to handle item click
     self.itemClick = function (element) {
         self.selectedLocation = element;
 
-        //error handling for JSONP
-        var wiki_timeout = setTimeout(function () {
-            showInfoWindow(element.name, "Cannot load data from wikipedia", "", element);
-        }, 8000);
+        //marker to center
+        map.setCenter(element.marker.getPosition());
+
+        //indicate marker
+        self.bounceMarker();
 
         //AJAX request for wiki data
         $.ajax({
@@ -136,14 +139,27 @@ function LocationsViewModel() {
             dataType: 'jsonp'
         }).done(
             function (x) {
-                var title = x[1][0];
-                var description = x[2][0];
-                var url = x[3][0];
+
+                var title = 'No title.';
+                if (x.length >= 2 && x[1].length > 0) {
+                    title = x[1][0];
+                }
+
+                var description = 'No description';
+                if (x.length >= 3 && x[2].length > 0) {
+                    description = x[2][0];
+                }
+
+                var url = 'No url available';
+                if (x.length >= 4 && x[3].length > 0) {
+                    url = x[3][0];
+                }
 
                 showInfoWindow(title, description, url, element);
 
-                clearTimeout(wiki_timeout);
-            });
+            }).fail(function (jqXHR, textStatus) {
+            showInfoWindow(element.name, 'Cannot load data from wikipedia', '', element);
+        });
     };
 
     // filter list
@@ -156,7 +172,7 @@ function LocationsViewModel() {
             //init markers
             if (map) {
                 self.locationsList().forEach(function (element) {
-                    element.marker.setMap(map);
+                    element.marker.setVisible(true);
                 });
             }
 
@@ -167,8 +183,8 @@ function LocationsViewModel() {
                 var result = item.name.toLowerCase().indexOf(filter) !== -1;
 
                 //init marker on map
-                if (result && item.marker && map) {
-                    item.marker.setMap(map);
+                if (result && item.marker) {
+                    item.marker.setVisible(true);
                 }
 
                 return result;
@@ -182,3 +198,10 @@ var locationsModel = new LocationsViewModel();
 
 //init bindings
 ko.applyBindings(locationsModel);
+
+//JQuery resize function for resizing map
+$(window).resize(function () {
+    var h = $(window).height(),
+        offsetTop = 60; // Calculate the top offset
+    $('#map').css('height', (h - offsetTop));
+}).resize();
